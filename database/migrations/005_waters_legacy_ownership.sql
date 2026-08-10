d151c7e88e8f7e56d7d6f3bd817d6245913a139c
Dirk <el_ninjo_is@yahoo.de>
2026-08-10 20:25:12 +0700

    005_waters_legacy_ownership.sql

--- database/migrations/005_waters_legacy_ownership.sql
+++ database/migrations/005_waters_legacy_ownership.sql
@@ -1,23 +1,20 @@
-– CatchTrack
-– Migration 005: Neutralize ambiguous legacy Waters ownership
-– Version: 5.0
-– Date: 2026-08-10
+-- CatchTrack
+-- Migration 005: Neutralize ambiguous legacy Waters ownership
+-- Version: 5.0
+-- Date: 2026-08-10
 
 PRAGMA foreign_keys = ON;
 
 UPDATE waters
-
 SET user_id = NULL
-
 WHERE
-user_id IS NOT NULL
-AND created_at <= (
-SELECT
-applied_at
-FROM migrations
-WHERE version = ‘4’
-LIMIT 1
-);
+    user_id IS NOT NULL
+    AND created_at <= (
+        SELECT applied_at
+        FROM migrations
+        WHERE version = '4'
+        LIMIT 1
+    );
 
 CREATE INDEX IF NOT EXISTS idx_waters_unassigned
 ON waters(user_id);
